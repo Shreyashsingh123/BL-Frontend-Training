9ace152578558d2cf6affc6a5ab8116c62c39465
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-12-27 23:09:15 +0530

    Added AverageAndSum.js , to take 5 random number and do their sum and average

---
+++ JS/AverageAndSum.js
@@ -0,0 +1,9 @@
+let a= Math.floor(Math.random() * 90) + 10;
+let b=Math.floor(Math.random()*90)+10;
+let c= Math.floor(Math.random() * 90) + 10;
+let d=Math.floor(Math.random()*90)+10;
+let e= Math.floor(Math.random() * 90) + 10;
+let sum=a+b+c+d+e;
+console.log("sum of these five number is"+" "+ sum);
+let avg=sum/5;
+console.log("Average of these five number is"+" "+ avg);
