c749a47db33e2f19b15e32d882c800f3a98554a7
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-12-28 15:40:56 +0530

    Added Harmonic.js , To find nth harmonic number using formula

--- JS/ForLoopPractice/Harmonic.js
+++ JS/ForLoopPractice/Harmonic.js
@@ -6,6 +6,7 @@ if(num>0){
     let h=0;
     for(let i=1;i<num;i++){
         h += 1/i;
+        // harmonic number=1+1/2+1/3+1/4....till nth 1/n
     }
     console.log(num+"th harmonic number is "+": "+h);
    
