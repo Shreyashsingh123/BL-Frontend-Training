ccfe97bdf8b1f0e1c99fa0bd75a82b8a46977b11
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-12-28 16:17:04 +0530

    Practiced HTML-CSS

--- JS/DiceAddition.js
+++
@@ -1,9 +0,0 @@
-let a=Math.floor(Math.random()*6+1);
-let b=Math.floor(Math.random()*6+1);
-console.log(a+b);
-
-/*or we can do with help of function
- const add = (d,e) => d + e;
-var ans = add(a,b);
-console.log(ans);
-*/
--- JS/Random.js
+++
@@ -1,3 +0,0 @@
-let a=Math.floor(Math.random()*10);
-console.log("This is a random number"+" "+a);
-// to generate a random number using math.random
