d763d072c815a8b9e5df098465e88041d49f31eb
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-12-28 13:06:44 +0530

    Added CoinFlip.js , To check whether  head or tails reach 11 wins first using while loop

--- JS/WhileLoop/CoinFlip.js
+++ JS/WhileLoop/CoinFlip.js
@@ -5,10 +5,10 @@ while(head<11 && tail<11){
     let flip=Math.random();
     // generate random num between 0 and 1 
     if(flip<0.5){
-        tail++;
+        head++;
     }
     else{
-        head++;
+        tail++;
     }
 }
 if(head==11){
