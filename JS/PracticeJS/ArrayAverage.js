bf89c253bf5aaf87569acf27b168f1d743ea33e7
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-12-29 14:35:30 +0530

    Added ArrayAverage.js , calculated the average of number stored in an array

---
+++ JS/PracticeJS/ArrayAverage.js
@@ -0,0 +1,13 @@
+
+const prompt=require("prompt-sync")();
+let num=Number(prompt("Enter size of array: "));
+let arr=[];
+for(let i=0;i<num;i++){
+    arr[i]=Number(prompt("enter element:"));
+}
+let sum=0;
+for(let i=0;i<num;i++){
+    sum+=arr[i];
+}
+let avg=sum/num;
+console.log(`Average of element of array is ${avg}`);
