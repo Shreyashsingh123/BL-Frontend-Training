b6762857c267a7e136161572ddc0ef619f682499
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-12-29 11:45:31 +0530

    Added Table1To10.js , To print table from 1 to 10

---
+++ JS/PracticeJS/Table1To10.js
@@ -0,0 +1,10 @@
+let i=1;
+let j=10;
+let start=1;
+console.log("tabl from 1 to 10 is :");
+while(start<=j){
+    for(let i=1;i<=j;i++){
+        console.log(`${start} * ${i}= ${start*i}`)
+    }
+    start++;
+}
