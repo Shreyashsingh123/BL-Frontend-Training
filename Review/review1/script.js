3416159da94d59506013f295f4cd4c4211566dc9
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-01-01 13:52:14 +0530

    Created Responsive layout using HTML and CSS and perform  basic JS operation

---
+++ Review/review1/script.js
@@ -0,0 +1,27 @@
+
+// document.getElementById("btn").addEventListener("click", function () {
+//     // this.classList.toggle("btn-clicked");
+//     document.getElementById("h").innerText="Footer";
+//     document.getElementById("h").style.backgroundColor="lightblue";
+//     document.getElementById("h").style.height="50px";
+//     document.getElementById("footer").innerText="Header";
+//     document.getElementById("footer").style.backgroundColor="lightpink";
+//     document.getElementById("footer").style.height="50px";
+//     document.getElementById("footer").style.fontSize="50px";
+//     document.getElementById("left").innerText="Right";
+//     document.getElementById("left").style.backgroundColor="lightgreen";
+//     document.getElementById("left").style.fontSize="50px";
+//     document.getElementById("right").innerText="Left";
+//     document.getElementById("right").style.backgroundColor="yellow";
+//     document.getElementById("right").style.fontSize="50px";
+    
+//     document.getElementById("main").style.backgroundColor="blue";
+    
+//     // document.getElementsByClassName("container").style;
+//   });
+
+function solve() {
+  document.body.classList.toggle("mobile-view");
+}
+
+
